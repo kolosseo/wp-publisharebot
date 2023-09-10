d99de23e3e4aca9b54c141588a6b2dfc086f7239
Jacopo Pace <my.root.access@gmail.com>
2023-09-10 23:14:42 +0200

    improve empty messages rejection
    (at least image or text)

--- wp-publisharebot.php
+++ wp-publisharebot.php
@@ -233,10 +233,10 @@ function pushbot_endpoint_put_cb( WP_REST_Request $request ) {
 	}
 
 	// Do nothing if no text
-	if ( ! $payload['text'] ) {
+	if ( ! $payload['text'] || ! $payload['base64Photo'] ) {
 		return new WP_Error(
-			'no_text',
-			'Missing "text"',
+			'no_text_nor_image',
+			'Missing "text" nor "image"',
 			['status' => 400]
 		);
 	}
